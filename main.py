18f5ceb7cf6b4d2effe3b8eb861cf720216b5a48
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-09-17 16:08:00 +0200

    added base menu

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from tkinter import *
 
+# --- CONSTANTS ---
+PRIMARY_COLOR = "#077187"
+
+
+class App:
+    def __init__(self, window):
+        # prepare window
+        self.window = window
+        window.title("Typing speed meter")
+        window.geometry("500x600")
+        window["bg"] = PRIMARY_COLOR
+        # prepare widgets
+        self.title_label = Label(window, text="Typing Speed Meter", bg=PRIMARY_COLOR, fg="white", font=("Comic Sans MS", 20, "bold"))
+
+        self.menu()
+
+
+    def menu(self):
+        self.title_label.pack()
+
 
 if __name__ == "__main__":
-    pass
+    root = Tk()
+    app = App(root)
+    root.mainloop()
